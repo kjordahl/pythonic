a661cae83ae3590ccaf329288c15b22681fe14a9
Kelsey Jordahl <kjordahl@enthought.com>
2013-05-06 20:15:51 -0400

    Add reverse slicing function

--- b_index_vs_reversed.py
+++ b_index_vs_reversed.py
@@ -8,6 +8,10 @@ def xrange_lookup():
     for i in xrange(len(colors)-1, -1, -1):
         foo = colors[i]
 
+def sliced():
+    for color in colors[::-1]:
+        foo = color
+
 def iterator():
     for color in reversed(colors):
         foo = color
@@ -16,4 +20,5 @@ if __name__ == '__main__':
     import timeit
     print("index_lookup = ", timeit.timeit("index_lookup()", setup="from __main__ import index_lookup"))
     print("xrange_lookup = ", timeit.timeit("xrange_lookup()", setup="from __main__ import xrange_lookup"))
+    print("sliced = ", timeit.timeit("sliced()", setup="from __main__ import sliced"))
     print("iterator = ", timeit.timeit("iterator()", setup="from __main__ import iterator"))
